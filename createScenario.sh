c3e8c30b6b91487ddddeccf39ec8c01ac0e64af8
v.loureiro <v.loureiro@l067.lab.dit.upm.es>
2021-01-04 21:09:02 +0100

    ensure correct creation

--- createScenario.sh
+++ createScenario.sh
@@ -9,6 +9,7 @@ VNX_DIR=~/../../mnt/tmp/openstack_lab-stein_4n_classic_ovs-v06
 
 /lab/cnvr/bin/get-openstack-tutorial.sh
 cd /mnt/tmp/openstack_lab-stein_4n_classic_ovs-v06
+sudo vnx -f openstack_lab.xml --destroy
 sudo vnx -f openstack_lab.xml --create
 sudo vnx -f openstack_lab.xml -x start-all,load-img
 sudo vnx_config_nat ExtNet $EXT
