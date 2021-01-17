56614d64e01975c0697f84b6a50469aa4b7649c8
v.loureiro <v.loureiro@l082.lab.dit.upm.es>
2021-01-17 17:32:37 +0100

    update deleteScenario

--- deleteScenario.sh
+++ deleteScenario.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Obtenemos la ruta absoluta del proyecto
+# Borramos el stack
 openstack stack delete stack_cnvr
+# Destruimos el escenario
 cd /mnt/tmp/openstack_lab-stein_4n_classic_ovs-v06/
 sudo vnx -f openstack_lab.xml --destroy
