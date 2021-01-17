688dfa406a3f55c357c824efac05c801094c5363
alejandro.vargasp <alejandro.vargasp@l198.lab.dit.upm.es>
2021-01-17 18:46:13 +0100

    added floating ips

--- createScenario.sh
+++ createScenario.sh
@@ -16,24 +16,14 @@ sudo vnx_config_nat ExtNet $EXT
 
 # Creamos nuevas imagenes con distintas configuraciones en funcion del servidor
 # Xenial image with mongo
-#source admin-openrc.sh
-#openstack image create --public --disk-format qcow2 --container-format bare --file BBDD-mongo mongo-BBDD
+source admin-openrc.sh
+openstack image create --public --disk-format qcow2 --container-format bare --file mongoVM.part mongoVM
 
 # Arrancamos el escenario
 cp $ABS_DIR/openstack_lab.xml $VNX_DIR
 cd $VNX_DIR
 
 sudo vnx -f openstack_lab.xml -x create-final-scenario
-
-
-
-# Configurar el LBaaS y el FWaas
-# Primero hay que entrar automáticamente en network
-#slogin network
-# Despues se configuran los ficheros neutron.conf y fwaas_driver.ini de neutron del Network
-#cd /etc/neutron/
-#awk '/service_plugins = / {$3="router,lbaasv2,firewall_v2"}{print}' neutron.conf > tmp.conf && mv tmp.conf neutron.conf
-#awk '/service_plugins = / {print $1, $2, $3}' neutron.conf
-#awk '/#service_provider / {$1="service_provider"}{print}' neutron_fwaas.conf > tmp.conf && mv tmp.conf neutron_fwaas.conf
-#awk '/service_provider / {$2="= FIREWALL:Iptables:neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver:default"}{print}' neutron_fwaas.conf > tmp.conf && mv tmp.conf neutron_fwaas.conf
-#awk '/service_provider = / {print $1, $2, $3}' neutron_fwaas.conf
+#cd $ABS_DIR
+#source client-openrc.sh
+#openstack stack create -t configure_scneario.yml stack1
