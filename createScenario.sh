cedf43def8dde96cfb7fca2c0614a09c50deceef
alejandro.vargasp <alejandro.vargasp@l219.lab.dit.upm.es>
2021-01-23 10:28:40 +0100

    floating ip added

--- createScenario.sh
+++ createScenario.sh
@@ -20,11 +20,11 @@ cd $VNX_DIR
 
 sudo vnx -f openstack_lab.xml -x create-final-scenario
 
-# Creamos nuevas imagenes con distintas configuraciones en funcion del servidor
+# Importamos las imagenes con distintas configuraciones en funcion del servidor
 # Xenial image with mongo
-#cd $ABS_DIR
-#source admin-openrc.sh
-#openstack image create --public --disk-format qcow2 --container-format bare --file mongoVM.part mongoVM
+cd $ABS_DIR
+source admin-openrc.sh
+openstack image create --public --disk-format qcow2 --container-format bare --file mongoVM.part mongoVM
 
 # Arrancamos el escenario
 #source client-openrc.sh
