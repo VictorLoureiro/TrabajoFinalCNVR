7c10b91f1b2c0317b113473b7502321798d0ef0f
AlexVaPe <alez101010@gmail.com>
2021-01-30 19:25:01 +0000

    web added

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '10.1.1.20';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
