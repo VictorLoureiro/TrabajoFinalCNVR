b4a26f0605c70ff012bd5394096165b414ee3363
alejandro.vargasp <alejandro.vargasp@l247.lab.dit.upm.es>
2021-01-30 10:31:55 +0100

    correction

--- web_app/controllers/patient.js
+++ web_app/controllers/patient.js
@@ -2,37 +2,63 @@
 const Patient = require('../models/patient');
 
 exports.list = async function() {
-    // Rellene aqui ...
+    let pacientes = await Patient.find();
+    return pacientes;
 }
 
 exports.read = async function(patientId) {
-    // Rellene aqui ...
+    
+	let paciente = await Patient.findById(patientId);
+	//console.log(typeof(paciente));
+	console.log(paciente);
+	return paciente;
 }
 
 exports.create = async function(body) {
-    // Rellene aqui ...
+	let newDoc = new Patient(body);
+    let nuevoPaciente = await newDoc.save();
+    return nuevoPaciente;
 }
 
 exports.update= async function(patientId, body) {
-    // Rellene aqui ...
+	let paciente = await Patient.findOneAndUpdate({_id:patientId},body,{new:true});
+	return paciente;
+
 }
 
 exports.delete = async function(patientId) {
-    // Rellene aqui ...
+	let paciente = await Patient.deleteOne({_id:patientId});
+	return paciente;
 }
 
 exports.filterPatientsByCity = async function (city) {
-    // Rellene aqui ...
+    let pacientes = await Patient.find({city:city});
+    return pacientes;
+
 }
 
 exports.filterPatientsByDiagnosis = async function (diagnosis) {
-    // Rellene aqui ...
+    let pacientes = await Patient.find({"medicalHistory.diagnosis":diagnosis});
+    return pacientes;
+
 }
 
 exports.filterPatientsBySpeacialistAndDate = async function (specialist, sDate,fDate) {
-    // Rellene aqui ...
+	
+	let pacientes = await Patient.aggregate([
+		{$unwind: "$medicalHistory"},
+		{$match:{"medicalHistory.specialist": specialist, "medicalHistory.date":{$gte: new Date(sDate),$lte: new Date(fDate)}}}
+	]);
+	console.log(pacientes)
+	return pacientes;
+
+    
 }
 
 exports.addPatientHistory = async function (patientId, medicalRecord) {
-    // Rellene aqui ...
+	let patient = await Patient.findById(patientId);
+	patient.medicalHistory.push(medicalRecord);
+	let result = await paciente.save();
+	return result;
+
 }
--- web_app/models/patient.js
+++ web_app/models/patient.js
@@ -1,64 +1,18 @@
-// Cargamos los modelos para usarlos posteriormente
-const Patient = require('../models/patient');
+'use strict'
+const mongoose =  require('mongoose');
+const Schema = mongoose.Schema;
 
-exports.list = async function() {
-    let pacientes = await Patient.find();
-    return pacientes;
-}
+const PatientSchema = Schema({
+    name: String,
+    surname: String,
+    dni: String, 
+    city: String,
+    profession: Array,
+    medicalHistory: [{
+    	specialist: String,
+    	diagnosis: String,
+    	date: Date,
+    }]
+});
 
-exports.read = async function(patientId) {
-    
-	let paciente = await Patient.findById(patientId);
-	//console.log(typeof(paciente));
-	console.log(paciente);
-	return paciente;
-}
-
-exports.create = async function(body) {
-	let newDoc = new Patient(body);
-    let nuevoPaciente = await newDoc.save();
-    return nuevoPaciente;
-}
-
-exports.update= async function(patientId, body) {
-	let paciente = await Patient.findOneAndUpdate({_id:patientId},body,{new:true});
-	return paciente;
-
-}
-
-exports.delete = async function(patientId) {
-	let paciente = await Patient.deleteOne({_id:patientId});
-	return paciente;
-}
-
-exports.filterPatientsByCity = async function (city) {
-    let pacientes = await Patient.find({city:city});
-    return pacientes;
-
-}
-
-exports.filterPatientsByDiagnosis = async function (diagnosis) {
-    let pacientes = await Patient.find({"medicalHistory.diagnosis":diagnosis});
-    return pacientes;
-
-}
-
-exports.filterPatientsBySpeacialistAndDate = async function (specialist, sDate,fDate) {
-	
-	let pacientes = await Patient.aggregate([
-		{$unwind: "$medicalHistory"},
-		{$match:{"medicalHistory.specialist": specialist, "medicalHistory.date":{$gte: new Date(sDate),$lte: new Date(fDate)}}}
-	]);
-	console.log(pacientes)
-	return pacientes;
-
-    
-}
-
-exports.addPatientHistory = async function (patientId, medicalRecord) {
-	let patient = await Patient.findById(patientId);
-	patient.medicalHistory.push(medicalRecord);
-	let result = await paciente.save();
-	return result;
-
-}
+module.exports = mongoose.model('Patient', PatientSchema);
