05885032d7b84feeca87668f5c88cb727722a5bd
alejandro.vargasp <alejandro.vargasp@l174.lab.dit.upm.es>
2021-01-31 18:39:33 +0100

    app mod

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const process = require('process');
 const hostIP = process.argv[2];
 const hostNAME = process.argv[3];
 const port = 80;
+const str1 = 'Hola! Estas en el Servidor ';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola! Estás en el Servidor ${hostNAME}');
+  res.end(str1.concat('',hostNAME);
 });
 
 server.listen(port, hostIP, () => {
