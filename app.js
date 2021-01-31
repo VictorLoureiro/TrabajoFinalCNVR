f0c320b07b3b2751add877dcedf358b899642a99
v.loureiro <v.loureiro@l123.lab.dit.upm.es>
2021-01-31 11:08:56 +0100

    js app update

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const http = require('http');
+const process = require('process');
 
-const hostname = '10.1.1.20';
+const hostIP = process.argv[2];
+const hostNAME = process.argv[3];
 const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo');
+  res.end('Hola! Estás en el Servidor ${hostNAME}');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+server.listen(port, hostIP, () => {
+  console.log(`El servidor se está ejecutando en http://${hostIP}:${port}/`);
 });
